47b8442c5d4fa87a600e4e3b78290f3bf18b8a77
jorge <jorgencnc0608@outlook.com>
2023-03-13 16:31:34 -0400

    change config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const apiUrl = process.env.NODE_ENV === 'development' 
     ? 'http://localhost:3000/api' // development api
-    : 'https://bloom-chi.vercel.app/api'; // production api
+    : 'https://bloom1.vercel.app/api'; // production api
 
 export {
     apiUrl
--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:3000/api' // development api
-            : 'https://bloom-chi.vercel.app/api' // production api
+            : 'https://bloom1.vercel.app/api' // production api
     },
     webpack5: true,
     webpack: (config) => {
