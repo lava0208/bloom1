40ebec001b11917861347a6046daac0d900daf26
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-24 16:52:12 -0400

    Update plantings.js

--- pages/api/plantings.js
+++ pages/api/plantings.js
@@ -58,18 +58,19 @@ function createTasks(planting, plant, plan){
                 break;
         }
         pinch_date = moment(seed_indoors_date).add(_pinch, 'days').format('YYYY/MM/DD');
-        pot_on_date = moment(seed_indoors_date).add(_pot_on, 'days').format('YYYY/MM/DD');
-        bloom_start_date = moment(seed_indoors_date).add(average_maturity, 'days').format('YYYY/MM/DD');
-        _harvest_duration = Math.round(moment(first_frost).diff(moment(bloom_start_date), 'days'));
-        harvest_date = moment(seed_indoors_date).add(moment(bloom_start_date, 'days')).format('YYYY/MM/DD');
+pot_on_date = moment(seed_indoors_date).add(_pot_on, 'days').format('YYYY/MM/DD');
+bloom_start_date = moment(seed_indoors_date).add(average_maturity, 'days').format('YYYY/MM/DD');
+_harvest_duration = Math.round(moment(first_frost).diff(moment(bloom_start_date), 'days'));
+harvest_date = moment(seed_indoors_date).add(_harvest_duration, 'days').format('YYYY/MM/DD');
+
 
     }else{
         direct_seed_date = moment(last_frost).add(_direct_sow, 'days').format('YYYY/MM/DD');
         pinch_date = moment(direct_seed_date).add(_pinch, 'days').format('YYYY/MM/DD');
         pot_on_date = moment(direct_seed_date).add(_pot_on, 'days').format('YYYY/MM/DD');
         bloom_start_date = moment(direct_seed_date).add(average_maturity, 'days').format('YYYY/MM/DD');
-        _harvest_duration = Math.round(moment(first_frost).diff(moment(bloom_start_date), 'days'));
-        harvest_date = moment(direct_seed_date).add(moment(bloom_start_date, 'days')).format('YYYY/MM/DD');
+_harvest_duration = Math.round(moment(first_frost).diff(moment(bloom_start_date), 'days'));
+harvest_date = moment(direct_seed_date).add(_harvest_duration, 'days').format('YYYY/MM/DD');
 
     }
     let harden_date = moment(last_frost).add(_harden, 'days').format('YYYY/MM/DD');
