bc2f2bd46c4d24b62642aa3a987f88208a7b882c
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-24 15:52:53 -0400

    Update plantings.js

--- pages/api/plantings.js
+++ pages/api/plantings.js
@@ -62,8 +62,8 @@ function createTasks(planting, plant, plan){
         pinch_date = moment(seed_indoors_date).add(_pinch, 'days').format('YYYY/MM/DD');
         pot_on_date = moment(seed_indoors_date).add(_pot_on, 'days').format('YYYY/MM/DD');
         harvest_date = moment(seed_indoors_date).add(_maturity_early, 'days').format('YYYY/MM/DD');
-        let average_maturity = Math.round((_maturity_early + _maturity_late) / 2);
-        let bloom_start_date = moment(seed_indoors_date).add(average_maturity, 'days');
+        average_maturity = Math.round((_maturity_early + _maturity_late) / 2);
+        bloom_start_date = moment(seed_indoors_date).add(average_maturity, 'days');
         _harvest_duration = plant.rebloom ? moment(first_frost).diff(bloom_start_date, 'days') : _maturity_late - _maturity_early;
 
     }else{
@@ -72,9 +72,9 @@ function createTasks(planting, plant, plan){
         pot_on_date = moment(direct_seed_date).add(_pot_on, 'days').format('YYYY/MM/DD');
         harvest_date = moment(direct_seed_date).add(_maturity_early, 'days').format('YYYY/MM/DD');
 
-        let average_maturity = Math.round((_maturity_early + _maturity_late) / 2);
-        let bloom_start_date = moment(direct_seed_date).add(average_maturity, 'days');
-        _harvest_duration = plant.rebloom ? moment(first_frost).diff(bloom_start_date, 'days') : _maturity_late - _maturity_early;
+    average_maturity = Math.round((_maturity_early + _maturity_late) / 2);
+    bloom_start_date = moment(seed_indoors_date).add(average_maturity, 'days');
+    _harvest_duration = plant.rebloom ? moment(first_frost).diff(bloom_start_date, 'days') : _maturity_late - _maturity_early;
     }
     let harden_date = moment(last_frost).add(_harden, 'days').format('YYYY/MM/DD');
     let transplant_date = moment(last_frost).add(_transplant, 'days').format('YYYY/MM/DD');
