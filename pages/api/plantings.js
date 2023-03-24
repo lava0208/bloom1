3b5be75d5e9fd94ab135ea9d1f0bab4cb8167276
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-24 16:07:49 -0400

    Update plantings.js

--- pages/api/plantings.js
+++ pages/api/plantings.js
@@ -43,7 +43,7 @@ function createTasks(planting, plant, plan){
     //... schedule dates
     let cold_stratify_date = moment(last_frost).subtract(_cold_stratify, 'days').format('YYYY/MM/DD');
     let pot_on_date;
-    let harvest_date;
+    let harvest_date = moment(last_frost).add(_harvest_duration, 'days').format('YYYY/MM/DD');
     let seed_indoors_date;
     let direct_seed_date;
     let pinch_date;
@@ -61,15 +61,12 @@ function createTasks(planting, plant, plan){
         }
         pinch_date = moment(seed_indoors_date).add(_pinch, 'days').format('YYYY/MM/DD');
         pot_on_date = moment(seed_indoors_date).add(_pot_on, 'days').format('YYYY/MM/DD');
-        harvest_date = moment(seed_indoors_date).add(_maturity_early, 'days').format('YYYY/MM/DD');
-        _harvest_duration = plant.rebloom ? moment(first_frost).diff(bloom_start_date, 'days') : _maturity_late - _maturity_early;
 
     }else{
         direct_seed_date = moment(last_frost).add(_direct_sow, 'days').format('YYYY/MM/DD');
         pinch_date = moment(direct_seed_date).add(_pinch, 'days').format('YYYY/MM/DD');
         pot_on_date = moment(direct_seed_date).add(_pot_on, 'days').format('YYYY/MM/DD');
-        harvest_date = moment(direct_seed_date).add(_maturity_early, 'days').format('YYYY/MM/DD');
-        _harvest_duration = plant.rebloom ? moment(first_frost).diff(bloom_start_date, 'days') : _maturity_late - _maturity_early;
+
     }
     let harden_date = moment(last_frost).add(_harden, 'days').format('YYYY/MM/DD');
     let transplant_date = moment(last_frost).add(_transplant, 'days').format('YYYY/MM/DD');
