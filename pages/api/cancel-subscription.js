ae29afc28696178e5b50a7f423682d5143666106
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-25 18:32:34 -0400

    Create cancel-subscription.js

---
+++ pages/api/cancel-subscription.js
@@ -0,0 +1,35 @@
+// Import the necessary modules
+import Stripe from "stripe";
+
+// Initialize the Stripe object with your secret key
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// Export the default async function to handle the request and response
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+    return;
+  }
+
+  const { customerId } = req.body; // or subscriptionId, depending on your implementation
+
+  try {
+    // Retrieve the subscriptions
+    const subscriptions = await stripe.subscriptions.list({
+      customer: customerId,
+    });
+
+    // Cancel the first active subscription found (assuming one subscription per customer)
+    for (const subscription of subscriptions.data) {
+      if (subscription.status === "active" || subscription.status === "trialing") {
+        await stripe.subscriptions.del(subscription.id);
+        break;
+      }
+    }
+
+    res.status(200).json({ message: "Subscription cancelled successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred while cancelling the subscription", error: error.message });
+  }
+}
