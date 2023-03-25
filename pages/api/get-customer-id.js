03ede319d8fc2f9db066ccfbf2f126fadba255d3
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-25 19:07:41 -0400

    Create get-customer-id.js

---
+++ pages/api/get-customer-id.js
@@ -0,0 +1,19 @@
+// /pages/api/get-customer-id.js
+
+import { stripe } from "services/stripe";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { sessionId } = req.body;
+
+    try {
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+      res.status(200).json({ customerId: session.customer });
+    } catch (error) {
+      res.status(400).json({ message: "Error retrieving customer ID", error });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
