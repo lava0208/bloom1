0884d65ddb2c5738086d2e5635bad9bb3700907a
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-26 11:18:00 -0400

    more error code

--- pages/api/verify-checkout-session.js
+++ pages/api/verify-checkout-session.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     try {
       // Retrieve the Checkout Session
       const session = await stripe.checkout.sessions.retrieve(sessionId);
-      console.log(session);
+      console.log("Stripe session response:", session); // Log the raw response
 
       // Check if the payment was successful
       if (session.payment_status === "paid") {
@@ -24,6 +24,8 @@ export default async function handler(req, res) {
         if (!updatedUser) {
           throw new Error(`Failed to update user with ID: ${user._id}`);
         }
+
+        console.log("Updated user:", updatedUser); // Log the updated user object
 
         res.status(200).json({ success: true });
       } else {
