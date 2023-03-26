2ecec9632c1c5e601ecf5ce593fac1acabdbd72c
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-26 12:25:20 -0400

    Initiating Change to Subscription Here

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,3 +1,5 @@
+// /pages/api/create-checkout-session.js
+import { getSession } from "next-auth/client";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.NEXT_SECRET_API_KEY);
@@ -7,20 +9,21 @@ export default async function handler(req, res) {
     try {
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
+        mode: "subscription",
         line_items: [
           {
-            price: process.env.NEXT_PRICE_ID,
+            price: "price_1MpjF2EVmyPNhExzk8OzvcVy",
             quantity: 1,
           },
         ],
-        mode: "subscription",
         success_url: `${req.headers.origin}/account/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: req.headers.origin,
       });
 
       res.status(200).json({ sessionId: session.id });
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      console.error("Error creating checkout session:", error);
+      res.status(500).json({ error: "Error creating checkout session" });
     }
   } else {
     res.setHeader("Allow", "POST");
