5f2446aed0319dbc277dd911358f6fbcb3182161
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-26 10:49:43 -0400

    return payment_ if non-funciton

--- pages/api/verify-checkout-session.js
+++ pages/api/verify-checkout-session.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
       const session = await stripe.checkout.sessions.retrieve(sessionId);
 
       // Check if the payment was successful
-      if (session.payment_status === "paid") {
+      if (session.status === "succeeded") {
         // Update the user's subscription status
         const user = await userService.getById(userId);
         user.share_custom_varieties = true;
