a2e971699f83b207d3a1c18e998e6b9340034bd7
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-28 08:04:41 -0400

    works?

--- pages/api/auth/user.js
+++ pages/api/auth/user.js
@@ -41,12 +41,12 @@ export default async function handler(req, res) {
                 },
                 {
                     $set: {
-                        name: req.body.data.name,
-                        email: req.body.data.email,
-                        password: bcrypt.hashSync(req.body.data.password, 10),
-                        email_newsletter: req.body.data.email_newsletter,
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: bcrypt.hashSync(req.body.password, 10),
+                        email_newsletter: req.body.email_newsletter,
                         share_custom_varieties: req.body.share_custom_varieties,
-                        profile_path: req.body.data.profile_path,
+                        profile_path: req.body.profile_path,
                         subscriptionId: req.body.subscriptionId
                     },
                 }
