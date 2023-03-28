4703ea23b4a9d9734e53bb970ba1a8f42ad40ba3
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-27 22:12:01 -0400

    Test

--- pages/api/stripe-webhook.js
+++ pages/api/stripe-webhook.js
@@ -1,71 +1,39 @@
+
+
 import Stripe from 'stripe';
-import { userService } from 'services';
+
 
 const stripe = new Stripe(process.env.NEXT_SECRET_API_KEY);
-const getRawBody = async (req) => {
-  const chunks = [];
-  for await (const chunk of req) {
-    chunks.push(chunk);
+
+module.exports = (req, res) => {
+  let event;
+
+  try {
+    // Verify the webhook signature
+    const signature = req.headers['stripe-signature'];
+    event = stripe.webhooks.constructEvent(req.body, signature, 'YOUR_STRIPE_WEBHOOK_SECRET');
+  } catch (err) {
+    console.log(`Webhook Error: ${err.message}`);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
   }
-  return Buffer.concat(chunks);
+
+  // Handle the checkout.session.completed event
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object;
+
+    // Retrieve the customer email and client reference ID from the completed session
+    const customerEmail = session.customer_email;
+    const clientId = session.client_reference_id;
+
+    // Do something with the customer email and client reference ID
+    console.log(`Checkout completed for client ${clientId} with email ${customerEmail}`);
+    
+    // Return a 200 response to acknowledge receipt of the event
+    return res.status(200).send('Checkout completed');
+  }
+
+  // Return a 200 response for any other event
+  return res.status(200).send('Received');
 };
 
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const sig = req.headers['stripe-signature'];
-    let event;
-
-    try {
-      const buf = await getRawBody(req);
-      event = stripe.webhooks.constructEvent(buf, sig, process.env.STRIPE_WEBHOOK_SECRET);
-    } catch (err) {
-      console.error('Webhook signature verification failed:', err.message);
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
-
-    if (event.type === 'checkout.session.completed') {
-      console.log('checkout.session.completed event received');
-      const session = event.data.object;
-      const userId = session.client_reference_id;
-      const user = await userService.getById(userId);
-      console.log('Fetched user:', user);
-      
-      // Retrieve the subscription from the session
-      const subscription = session.subscription;
-      console.log('subscription:', subscription);
-    
-      // Update the user object with the subscription ID and share_custom_varieties
-      const updatedUser = {
-        ...user,
-        share_custom_varieties: true,
-        subscriptionId: subscription,
-      };
-    
-      // Save the updated user object to MongoDB
-      await userService.update(userId, updatedUser);
-    }    
-    
-
-    if (event.type === 'customer.subscription.deleted') {
-      const subscription = event.data.object;
-      const userId = subscription.metadata.userId;
-      const user = await userService.getById(userId);
-      const updatedUser = { ...user, share_custom_varieties: false };
-      await userService.update(userId, updatedUser);
-    }
-
-    // Handle other subscription-related events here if necessary
-
-    res.status(200).json({ received: true });
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
-  }
-}
