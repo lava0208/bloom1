2073c59d9bc33b2f1226ce5df040d258f09980dc
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-27 20:42:36 -0400

    revert

--- config.js
+++ config.js
@@ -3,13 +3,3 @@ const apiUrl = 'https://app.bloommanager.com/api';
 export {
     apiUrl
 };
-
-module.exports = {
-    env: {
-      NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
-      NEXT_SECRET_API_KEY: process.env.NEXT_SECRET_API_KEY,
-      STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-      // Add any other environment variables you may have
-    },
-  };
-  
