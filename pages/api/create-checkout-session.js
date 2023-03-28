b9a51c1f2293bc71d017bd6aa3a78b5075367e5b
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-27 21:37:07 -0400

    add reference id

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
 
       const session = await stripe.checkout.sessions.create({
         customer_email: user.email, // Set the customer's email
+        client_reference_id: userId, // Set the client reference ID
         payment_method_types: ["card"],
         mode: "subscription",
         line_items: [
