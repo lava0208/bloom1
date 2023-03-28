2b2a467007c520f8f4303264e01ed4206cea2699
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-28 15:36:33 -0400

    style

--- pages/account/success.js
+++ pages/account/success.js
@@ -46,7 +46,7 @@ const Success = () => {
       <img className={styles.logo} src={"/assets/logo.png"} alt="logo" />
       <h2>You&apos;re good to go!</h2>
       {isSubscriptionActive ? (
-        <h3><b>Your PRO subscription is active!</b><br />Let&apos;s make this season the best ever.</h3>
+        <h3>Your <b>PRO</b> subscription is active! Let&apos;s make this season the best ever.</h3>
       ) : (
         <h3>Let&apos;s make this season the best ever.</h3>
       )}
