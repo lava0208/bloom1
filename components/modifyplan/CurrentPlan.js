aa5855a63e30a41c70799a6161a92434f17770b1
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-30 16:37:56 -0400

    Fix

--- components/modifyplan/CurrentPlan.js
+++ components/modifyplan/CurrentPlan.js
@@ -343,6 +343,19 @@ const CurrentPlan = (props) => {
                                     <></>
                                 )
                             }
+                            {
+                                 planting.bulb && plant.bulb_pot_on !== "" ? (
+                                    <div className={styles.successionCheckboxRow}>
+                                        <h6>Pot On</h6>
+                                        <div
+                                            onClick={() => {setPotCheckbox(!potCheckbox), setPlanting({...planting, bulb_pot_on: !potCheckbox})}}
+                                            className={`${styles.checkbox} ${potCheckbox ? styles.active : null}`}
+                                        ></div>
+                                    </div>
+                                ) : (
+                                    <></>
+                                )
+                            }
                         </div>
                     </div>
                 </div>
