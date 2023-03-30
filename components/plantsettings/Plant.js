aec8eccaeba5814da312ae737a8ed4498a1fec93
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-29 21:24:06 -0400

    try

--- components/plantsettings/Plant.js
+++ components/plantsettings/Plant.js
@@ -246,7 +246,7 @@ const Plant = (props) => {
             </div>
             <div className="row mt-4">
                 <div className={styles.inputContainer + " col-md-6"}>
-                <div className="col-md-6">
+                    <div className="col-md-6">
   <Nav tabs>
     <NavItem>
       <NavLink
@@ -311,7 +311,255 @@ const Plant = (props) => {
   </Nav>
   <TabContent activeTab={activeTab}>
     <TabPane tabId="1">
-      {/* Indoor Timing content */}
+    <h5>Indoor Timing</h5>
+                    <small>Earliest Seed (weeks before last frost)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.earliest_seed : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                earliest_seed: e.target.value,
+                            });
+                        }}
+                    />
+                    <small>Latest Seed (weeks before last frost)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.latest_seed : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                latest_seed: e.target.value,
+                            });
+                        }}
+                    />
+                    <small>Pinch (weeks after seeding indoors)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.pinch : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                pinch: e.target.value,
+                            });
+                        }}
+                    />
+                    <small>Pot On (weeks after seeding indoors)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.pot_on : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                pot_on: e.target.value,
+                            });
+                        }}
+                    />
+                    <small>Harden (weeks after last frost)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.harden : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                harden: e.target.value,
+                            });
+                        }}
+                    />
+                    <small>Transplant (weeks after last frost)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.transplant : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                transplant: e.target.value,
+                            });
+                        }}
+                    />
+                    <h5 className="mt-3">Harvest</h5>
+                    <small>Maturity Early (days after seeding)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.maturity_early : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                maturity_early: e.target.value,
+                            });
+                        }}
+                    />
+                    <small>Maturity Late (days after seeding)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.maturity_late : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                maturity_late: e.target.value,
+                            });
+                        }}
+                    />
+                    <h6 className="d-flex align-items-center">
+                        <label htmlFor="rebloom">Rebloom?</label>
+                        <input
+                            type="checkbox"
+                            id="rebloom"
+                            value={plant ? plant.rebloom : ""}
+                            checked={plant ? plant.rebloom : false}
+                            onChange={(e) => {
+                                setPlant({
+                                    ...plant,
+                                    rebloom: e.target.checked,
+                                });
+                            }}
+                        />
+                    </h6>
+                    <h5 className="mt-3">Transplant Note</h5>
+                    <textarea
+                        rows="3"
+                        value={plant ? plant.transplant_note : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                transplant_note: e.target.value,
+                            });
+                        }}
+                    />
+                    <h5 className="mt-3">Pinch Note</h5>
+                    <textarea
+                        rows="3"
+                        value={plant ? plant.pinch_note : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                pinch_note: e.target.value,
+                            });
+                        }}
+                    />
+                </div>
+                <div className={styles.inputContainer + " col-md-6"}>
+                    <h5>Direct Seed Timing</h5>
+                    <small>Direct Seed (weeks after last frost)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.direct_seed : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                direct_seed: e.target.value,
+                            });
+                        }}
+                    />
+                    <small>Pinch (weeks after direct seeding)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.direct_seed_pinch : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                direct_seed_pinch: e.target.value,
+                            });
+                        }}
+                    />
+                    <h5 className="mt-4">Seeding</h5>
+                    <small>Depth (mm)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.depth : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                depth: e.target.value,
+                            });
+                        }}
+                    />
+                    <small>Cold Stratify (weeks before seeding)</small>
+                    <input
+                        type="number"
+                        className={styles.input}
+                        value={plant ? plant.cold_stratify : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                cold_stratify: e.target.value,
+                            });
+                        }}
+                    />
+                    <h6 className="d-flex align-items-center">
+                        <label htmlFor="light">Light for germination</label>
+                        <input type="checkbox" id="light"
+                            value={plant ? plant.light : ""}
+                            checked={plant ? plant.light : false}
+                            onChange={(e) => {
+                                setPlant({
+                                    ...plant,
+                                    light: e.target.checked,
+                                });
+                            }}
+                        />
+                    </h6>
+                    <h5 className="mt-3">Indoor Seed Note</h5>
+                    <textarea
+                        rows="3"
+                        value={plant ? plant.indoor_seed_note : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                indoor_seed_note: e.target.value,
+                            });
+                        }}
+                    />
+                    <h5 className="mt-3">Direct Seed Note</h5>
+                    <textarea
+                        rows="3"
+                        value={plant ? plant.direct_seed_note : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                direct_seed_note: e.target.value,
+                            });
+                        }}
+                    />
+                    <h5>Pot On Note</h5>
+                    <textarea
+                        rows="3"
+                        value={plant ? plant.pot_on_note : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                pot_on_note: e.target.value,
+                            });
+                        }}
+                    />
+                    <h5>Harvest Note</h5>
+                    <textarea
+                        rows="3"
+                        value={plant ? plant.harvest_note : ""}
+                        onChange={(e) => {
+                            setPlant({
+                                ...plant,
+                                harvest_note: e.target.value,
+                            });
+                        }}
+                    />
+                </div>
+                <div className={styles.inputContainer + " text-center"}>
+                    <button onClick={() => { savePlant() }}>Save Changes</button>
+                    <button onClick={props.cancelPlant}>Cancel</button>
+                </div>
     </TabPane>
     <TabPane tabId="2">
       {/* Direct Seed Timing content */}
