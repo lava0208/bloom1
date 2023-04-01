1a889edea7fb38e3cb4dae0f3905bc2aba3761d0
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-31 20:54:05 -0400

    test

--- components/masterplan/List.js
+++ components/masterplan/List.js
@@ -85,10 +85,10 @@ const List = () => {
     style={{
       display: 'flex',
       justifyContent: 'center',
+      alignItems: 'center',
       width: '100%',
       height: '100%',
       minHeight: '100vh',
-      position: 'absolute',
       top: 0,
       left: 0,
     }}
