8ec3a7c7630a24f718ca7c75772de63ad884b3c3
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-31 20:43:22 -0400

    fix

--- components/masterplan/List.js
+++ components/masterplan/List.js
@@ -80,7 +80,21 @@ const List = () => {
         <div className={styles.container}>
 
 {loading ? (
-      <PuffLoader color="#00BFFF" size={100} />
+    <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: '100%',
+      minHeight: '100vh',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+    }}
+  >
+      <FadeLoader color="#505168" size={100} />
+      </div>
     ) : (
 
         <>
