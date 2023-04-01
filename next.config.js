ee96e5eb6ef5db964400f33350e82e6ed400cff3
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-03-31 22:48:20 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    development: true, // enable development mode
     swcMinify: false,
     serverRuntimeConfig: {
         secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
