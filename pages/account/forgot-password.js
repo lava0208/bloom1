e22e49b2dbe97f3765a8fe07aa9e7eec0b85e005
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-04-22 14:36:02 -0400

    style test

--- pages/account/forgot-password.js
+++ pages/account/forgot-password.js
@@ -1,10 +1,11 @@
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-import { userService } from '../../services/user.service';
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { userService } from "../../services/user.service";
+import styles from "~styles/pages/account/register.module.scss";
 
 export default function ForgotPassword() {
   const router = useRouter();
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   async function handleSubmit(e) {
@@ -13,7 +14,7 @@ export default function ForgotPassword() {
     const response = await userService.forgotPassword({ email });
     if (response.status) {
       alert(response.message);
-      router.push('/account/login');
+      router.push("/account/login");
     } else {
       alert(response.message);
       setSubmitted(false);
@@ -21,20 +22,31 @@ export default function ForgotPassword() {
   }
 
   return (
-    <div>
-      <h2>Forgot Password</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email Address"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <button type="submit" disabled={submitted}>
-          Submit
-        </button>
-      </form>
+    <div className={styles.screen}>
+      <img className={styles.logo} src={"/assets/logo.png"} alt="logo" />
+      <div className={styles.formContainer}>
+        <h2>Forgot Password</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className={styles.input}
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <div
+            className={styles.nextButtonContainer}
+            onClick={handleSubmit}
+            disabled={submitted}
+          >
+            <h5>Submit</h5>
+          </div>
+        </form>
+        <h4>
+          <a onClick={() => router.push("/account/login")}>Back to Login</a>
+        </h4>
+      </div>
     </div>
   );
 }
