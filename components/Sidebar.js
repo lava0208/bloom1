da845d10ac3e54c0ea748f0d19fd9603f4935652
noahlittle <61997107+noahlittle@users.noreply.github.com>
2023-04-30 14:20:00 -0400

    test

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -142,8 +142,8 @@ const Sidebar = (props) => {
                                     }                                    
                                 </div>
                                 <div className={styles.accountInfoContainer}>
-                                    {user.share_custom_varieties && <button className={styles.proButton}>PRO</button>}
                                     <h4 onClick={() => router.push("/profile")}>{user.name}</h4>
+                                    {user.share_custom_varieties && <button className={styles.proButton}>PRO MEMBER</button>}
                                     <h5 onClick={() => logout()}>Log Out</h5>
                                 </div>
                             </div>
